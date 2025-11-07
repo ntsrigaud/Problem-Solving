7b720151ffd43aed033851b392e5e321164488ed
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-11-07 11:33:11 +0800

    chore(UVA-122): problem solution template

---
+++ CPE/Trees-on-the-Level/include/solution.h
@@ -0,0 +1,19 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+/*
+ *
+ * Revision history:
+ * Revised by Neil Taison Rigaud, August 2025
+ *
+ * */
+
+class Solution {
+public:
+  Solution() = default;
+  ~Solution() = default;
+
+  // Add problem-specific methods here
+};
+
+#endif // !SOLUTION_H

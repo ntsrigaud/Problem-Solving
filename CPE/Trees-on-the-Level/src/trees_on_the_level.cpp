8fae35bbc46120bee3cb48b7ac2d099d8090bc81
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-11-09 09:29:39 +0800

    docs(UVA-122): new implementation procedure

--- CPE/Trees-on-the-Level/src/trees_on_the_level.cpp
+++ CPE/Trees-on-the-Level/src/trees_on_the_level.cpp
@@ -1,12 +1,4 @@
-#include <algorithm>
 #include <iostream>
-#include <memory>
-#include <queue>
-#include <stack>
-#include <stdexcept>
-#include <string>
-#include <unordered_map>
-#include <vector>
 
 /*
  * UVA: Trees on the level
@@ -20,5 +12,25 @@
  *
  * Implementation procedure
  * ------------------------
+ * 1) Retrieve nodes in main in sequences until you encounter '()'
+ *
+ * 2) Process sequence:
+ *    a) Validate sequence:
+ *       - Check if sequence contains a HEAD node
+ *          -> Stop and output "not complete"
+ *       - Check if sequence contains duplicate tree positions
+ *          -> Stop and output "not complete"
+ *       - Check if sequence misses tree positions
+ *          -> Stop and output "not complete"
+ *       - Check if sequence contains duplicate values
+ *          -> Update tree_complete_specification to false
+ *
+ *    b) Display level order traversal
+ *
+ *    c) If tree_complete_specication == false, then output "not complete\n"
+ *
+ * 3) Proceed to 1) until end of input
+ *
  * */
 
+int main() { return 0; }

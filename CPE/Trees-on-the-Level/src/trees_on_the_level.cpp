17f794c13d79d5fd3125c61c015563ce36453e8f
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-11-10 17:54:29 +0800

    docs(UVA-122): third implementation procedure

--- CPE/Trees-on-the-Level/src/trees_on_the_level.cpp
+++ CPE/Trees-on-the-Level/src/trees_on_the_level.cpp
@@ -18,5 +18,23 @@
  *
  * Implementation procedure
  * ------------------------
+ * 1) Retrieve and save input nodes:
+ *    - Parse the first node input from a sequence until no input is found
+ *    - Save the first node
+ *    - Parse the rest of the sequence until "()"
+ *      - Use a set to track duplicate node positions
+ *        -> Stop parsing the current sequence if at least one duplicate node is
+ * found
+ *          -> Display "not complete\n"
+ *          -> Proceed to the next input sequence directly
+ *      - Use a set to track node duplicate values
+ *        -> Tree is not completely specified.
+ *      - Check if all required tree positions are filled.
+ *        - Apart from HEAD, all children must have a parent.
+ *      - Display the levelOrderTraversal if tree is valid.
+ *        -> Sort the list in ascending order by their positions
+ *        -> Display the values of each node.
+ *        * Since the level order starts from LEFT to RIGHT and the sorted node
+ * list correspond to that order, we obtain the correct sequence just by sorting
+ * the list.
  * */
-

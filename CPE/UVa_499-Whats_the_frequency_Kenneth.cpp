2442a95ec1bf183f562541569913d943beb7131b
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-11-12 21:30:28 +0800

    feat(UVa-499): inital implementation - PASSED

---
+++ CPE/UVa_499-Whats_the_frequency_Kenneth.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+/*
+ * UVA: What's the Frequency, Kenneth?
+ * Link: https://onlinejudge.org/external/4/499.pdf
+ *
+ * Revision history:
+ * Originally written by Neil Taison Rigaud, November 2025
+ * */
+
+int main() {
+  std::unordered_map<char, int> record;
+  std::vector<char> res;
+  std::string line;
+  int max_freq = 0;
+
+  while (std::getline(std::cin, line)) {
+    max_freq = 0;
+    res.clear();
+    record.clear();
+
+    for (const auto &letter : line) {
+      if (!std::isalpha(letter))
+        continue;
+
+      record[letter] =
+          (record.find(letter) == record.end()) ? 1 : record[letter] + 1;
+      max_freq = (record[letter] > max_freq) ? record[letter] : max_freq;
+    }
+
+    for (const auto &p : record) {
+      if (p.second == max_freq) {
+        res.push_back(p.first);
+      }
+    }
+
+    std::sort(res.begin(), res.end());
+    line = std::string(res.begin(), res.end());
+    std::cout << line << " " << max_freq << '\n';
+  }
+
+  return 0;
+}

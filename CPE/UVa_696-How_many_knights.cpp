630574f743bf8efb360b9761d41bd5cff963a808
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-11-19 20:41:08 +0800

    feat(UVa-696): final solution - PASSED
    Used the geometric properties of the board to compute the solution by
    differentiating between the 3 cases:
    
    - Case 1: n = 1 or m = 1
      Since to move or attack a knight needs at least one row or column
    different than its position for displacement, we can place knights on
    all the available cells, they cannot attack each other.
    
    - Case 2: n == 2
      The same pattern is repeated by column or row of 4 in the maximum
    dimension (m), and the number of knights to put on the remainder rows or
    columns is deduced.
    
    - Case 3: n > 2 (General case)
      Since a knight moves from one color to the oposite, we can place a
    knight on the maximum cells of the same color, they will not be able to
    attack each other if they are on the same cell color. An integer trick
    (+1) is used to find the correct maximum cell color where in cases such
    as N=5 and M=5 we have 25 cells and dividing by would give the answer
    12, but we can have a maximum of 13 cells of the same color on a 5x5
    board to place knights. That is the reason of the (+1) in the result
    (n*m + 1) / 2 for the number of knights
    
    Signed-off-by: Neil Taison Rigaud <ntsrigaud14@gmail.com>

--- CPE/UVa_696-How_many_knights.cpp
+++ CPE/UVa_696-How_many_knights.cpp
@@ -4,159 +4,39 @@
 /*
  * UVA: How Many Knights
  * Link: https://onlinejudge.org/external/6/696.pdf
- *
- * Backtracking Procedure
- * ----------------------
- *  - Normalize the column index at the start of the recursion
- *    - If it is past end, advance the row
- *  - If we reach beyond the last row, return 0
- *  - Search for the next square using local loop variables
- *  - Try placing a knight, and compare with skipping the cell
+ * Ref.:
+ * https://github.com/marioyc/Online-Judge-Solutions/blob/master/UVA/VI/696%20-%20How%20Many%20Knights.cpp
  * */
 
-#define LOG(x) std::cout << "Log: " << x << '\n'
-
-constexpr int KNIGHT_RANGE = 2;
-constexpr char KNIGHT = 'N';
-constexpr char EMPTY = ' ';
-
-struct Coord {
-  int row;
-  int col;
-};
-
-struct Dim : Coord {
-  Dim() : Coord{0, 0} {};
-  Dim(int m, int n) : Coord{m, n} {};
-  Dim(const Dim &d) : Coord{d.row, d.col} {};
-};
-
-class ChessBoard {
-private:
-  std::vector<std::vector<char>> board;
-  Dim dim;
-
-public:
-  [[nodiscard]] Dim get_dim() const { return dim; }
-
-  explicit ChessBoard(const Dim &d) : dim(d) {
-    board.resize(dim.row, std::vector<char>(dim.col, EMPTY));
-  };
-
-  [[nodiscard]] bool isUnderAttack(const Coord &coord) const {
-    auto in_row = [&](int r) -> bool { return r >= 0 && r < dim.row; };
-    auto in_col = [&](int c) -> bool { return c >= 0 && c < dim.col; };
-
-    // 2 rows UP, 1 column RIGHT and 1 column LEFT
-    if (in_row(coord.row - KNIGHT_RANGE)) {
-      int r = coord.row - KNIGHT_RANGE;
-
-      if (in_col(coord.col - 1) && board.at(r).at(coord.col - 1) == KNIGHT) {
-        return true;
-      }
-
-      if (in_col(coord.col + 1) && board.at(r).at(coord.col + 1) == KNIGHT) {
-        return true;
-      }
-    }
-
-    // 2 rows DOWN, 1 column RIGHT and 1 column LEFT
-    if (in_row(coord.row + KNIGHT_RANGE)) {
-      int r = coord.row + KNIGHT_RANGE;
-
-      if (in_col(coord.col - 1) && board.at(r).at(coord.col - 1) == KNIGHT) {
-        return true;
-      }
-
-      if (in_col(coord.col + 1) && board.at(r).at(coord.col + 1) == KNIGHT) {
-        return true;
-      }
-    }
-
-    // 2 cols LEFT, 1 row UP and 1 row DOWN
-    if (in_col(coord.col - KNIGHT_RANGE)) {
-      int c = coord.col - KNIGHT_RANGE;
-
-      if (in_row(coord.row - 1) && board.at(coord.row - 1).at(c) == KNIGHT) {
-        return true;
-      }
-
-      if (in_row(coord.row + 1) && board.at(coord.row + 1).at(c) == KNIGHT) {
-        return true;
-      }
-    }
-
-    // 2 cols RIGHT, 1 row UP and 1 row DOWN
-    if (in_col(coord.col + KNIGHT_RANGE)) {
-      int c = coord.col + KNIGHT_RANGE;
-
-      if (in_row(coord.row - 1) && board.at(coord.row - 1).at(c) == KNIGHT) {
-        return true;
-      }
-
-      if (in_row(coord.row + 1) && board.at(coord.row + 1).at(c) == KNIGHT) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  void printBoard() {
-    for (int i = 0; i < dim.row; ++i) {
-      for (int j = 0; j < dim.col; ++j) {
-        std::cout << (board.at(i).at(j) == KNIGHT ? 'N' : ' ') << "|";
-      }
-      std::cout << '\n';
-    }
-  }
-
-  int addKnights(Coord c) {
-    // Normalize coordinate (so col can overflow and move to next row)
-    if (c.col >= dim.col) {
-      c.col = 0;
-      c.row++;
-    }
-
-    if (c.row >= dim.row) {
-      return 0;
-    }
-
-    // Find next safe cell starting from c
-    for (int r = c.row; r < dim.row; ++r) {
-      for (int col = (r == c.row ? c.col : 0); col < dim.col; ++col) {
-        if (board[r][col] != KNIGHT && !isUnderAttack({r, col})) {
-          // try placing a knight here
-          board[r][col] = KNIGHT;
-          // next coordinate is same row, next column
-          Coord next{r, col + 1};
-          int with = 1 + addKnights(next);
-          // undo
-          board[r][col] = EMPTY;
-          // skip this cell
-          int without = addKnights(next);
-
-          return std::max(with, without);
-        }
-      }
-    }
-
-    // no safe cell found
+int solve(int n, int m) {
+  if (m == 0 || n == 0) {
     return 0;
   }
-};
+  if (n > m) {
+    std::swap(n, m);
+  }
+  if (n == 1) {
+    // No col to jump
+    return m;
+  }
+
+  if (n == 2) {
+    // Repeated pattern
+    return ((m / 4) * 4) + (m % 4 == 1 ? 2 : (m % 4 >= 2) ? 4 : 0);
+  }
+
+  return (n * m + 1) / 2; // Integer trick
+}
 
 int main() {
-  Dim b_dim;
-  Coord c{0, 0};
+  int m = 0;
+  int n = 0;
+  int knights = 0;
 
-  while (std::cin >> b_dim.row >> b_dim.col && b_dim.row != 0 &&
-         b_dim.col != 0) {
-    ChessBoard board(b_dim);
-    int n_knights = board.addKnights(c);
-
-    std::cout << n_knights << " knights may be placed on a " << b_dim.row
-              << " row " << b_dim.col << " column board.\n";
+  while (std::cin >> m >> n && (m + n) != 0) {
+    knights = solve(m, n);
+    std::cout << knights << " knights may be placed on a " << m << " row " << n
+              << " column board.\n";
   }
 
   return 0;

46ae237c2af4ad3b8b0aacecec0ccafbf53d5e6e
Neil Taison RIGAUD <ntsrigaud14@gmail.com>
2026-01-31 14:53:46 +0800

    feat(factorial): initial basic and limited implementation
    Closes #25

---
+++ CPP-MASTERY-CURRICULUM/src/factorial.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdexcept>
+
+// Write `int factorial(int n)` that computes n!
+
+// Max value before overflow for this implementation
+constexpr int MAX_INT_VALUE = 12;
+
+int factorial(int n) {
+  if (n < 0) {
+    throw std::invalid_argument("Factorial of negative input.");
+  }
+
+  if (n > MAX_INT_VALUE) {
+    throw std::runtime_error("Operation overlfow. Please try a smaller value.");
+  }
+
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+
+  int a = 1;
+  int b = 1;
+
+  for (int i = 2; i <= n; ++i) {
+    a = b;
+    b = a * i;
+  }
+
+  return b;
+}
+
+int main() {
+  try {
+    int n = 0;
+
+    std::cout << "Please enter a number between 0 and 12: ";
+    if (!(std::cin >> n)) {
+      throw std::invalid_argument("Please enter a valid numeric value.");
+    }
+
+    std::cout << "Factorial(" << n << ") = " << factorial(n) << '\n';
+  } catch (const std::invalid_argument &e) {
+    std::cerr << "Invalid argument: " << e.what() << '\n';
+  } catch (const std::runtime_error &e) {
+    std::cerr << "Runtime error: " << e.what() << '\n';
+  } catch (...) {
+    std::cerr << "An unexpected error has occurred.\n";
+  }
+
+  return 0;
+}

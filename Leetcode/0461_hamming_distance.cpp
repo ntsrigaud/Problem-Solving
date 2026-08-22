937511a946264822bc1baf1ae97b932d552552a2
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2026-08-22 23:28:40 +0800

    refactor(lc-0461): replace `str` operations with bitwise XOR operator

--- Leetcode/0461_hamming_distance.cpp
+++ Leetcode/0461_hamming_distance.cpp
@@ -1,8 +1,6 @@
 // Leetcode 0461: Hamming Distance
 
-#include <algorithm>
 #include <cstddef>
-#include <string>
 
 class Solution {
 public:
@@ -12,39 +10,11 @@ public:
     }
 
     int distance = 0;
+    int diff = x ^ y; // Assuming that they will all be >= 0
 
-    auto toBin = [](int n) -> std::string {
-      std::string res;
-
-      while (n > 0) {
-        res += static_cast<char>((n % 2) + '0');
-        n /= 2;
-      }
-
-      std::ranges::reverse(res.begin(), res.end());
-      return res;
-    };
-
-    auto x_bin = toBin(x);
-    auto y_bin = toBin(y);
-
-    size_t pad_len =
-        std::abs(static_cast<int>(x_bin.length() - y_bin.length()));
-
-    if (pad_len > 0) {
-      std::string pad(pad_len, '0');
-
-      if (x_bin.length() < y_bin.length()) {
-        x_bin = pad + x_bin;
-      } else {
-        y_bin = pad + y_bin;
-      }
-    }
-
-    for (size_t i = 0; i < x_bin.length(); ++i) {
-      if (x_bin[i] != y_bin[i]) {
-        ++distance;
-      }
+    while (diff > 0) {
+      distance += diff % 2;
+      diff /= 2;
     }
 
     return distance;
